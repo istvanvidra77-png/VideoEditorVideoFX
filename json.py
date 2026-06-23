50c2b367f5d7a9d54212345f387f5ecee8ab8924
istvanvidra77-png <istvanvidra77@gmail.com>
2026-06-23 22:35:26 +0200

    Add JSON configuration file for video processing

---
+++ json.py
@@ -0,0 +1,8 @@
+{
+  "ffmpeg_path": "/usr/bin/ffmpeg",
+  "ffprobe_path": "/usr/bin/ffprobe",
+  "temp_dir": "./temp",
+  "max_workers": 4,
+  "default_quality": "high",
+  "supported_formats": ["mp4", "mov", "avi", "webm", "mkv"]
+}
